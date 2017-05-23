375bab253f1e1fb6e1d89c858c0fcfa4addae560
ferealqq <s1500631@student.businesscollege.fi>
2017-05-23 15:44:50 +0300

    checkpassword works fine and added the method for closing connection

--- src/ahk/pkginterface/database/ProfileDB.java
+++ src/ahk/pkginterface/database/ProfileDB.java
@@ -14,7 +14,7 @@ public class ProfileDB {
     private Statement st = null;
     private PreparedStatement prepStatement = null;
     private String[] connectionStrings = {"","",""};
-    
+
     public ProfileDB() {
         // tämä on testi
         connectionStrings[0] = "jdbc:postgresql://localhost:5432/ahk-interface";
@@ -23,24 +23,44 @@ public class ProfileDB {
     }
 /**
  *  Creates the user and sends the data to the database server
- */    
+ */
     public boolean createUser(String username,String password,String email){
         if(!isValidEmailAddress(email)) return false;
         password = BCrypt.hashpw(password, BCrypt.gensalt());
         try{
             connection = DriverManager.getConnection(connectionStrings[0],connectionStrings[1],connectionStrings[2]);
-            String sqlquery = "Insert into profile (username,password,email) values (?,?,?)"; 
+            String sqlquery = "Insert into profile (username,password,email) values (?,?,?)";
             prepStatement = connection.prepareStatement(sqlquery);
             prepStatement.setString(1,username);
             prepStatement.setString(2,password);
             prepStatement.setString(3,email);
             prepStatement.executeUpdate();
             prepStatement.close();
-            connection.close();
             return true;
         }catch (Exception ex){
             System.out.println("Error in createUser : "+ ex);
             return false;
+        }finally {
+            closeConnection(connection);
+        }
+    }
+    public boolean checkPassword(String password,int currentUserId){
+        try{
+            connection=DriverManager.getConnection(connectionStrings[0],connectionStrings[1],connectionStrings[2]);
+            String sqlquery = "select password from profile where profile_id=?";
+            prepStatement = connection.prepareStatement(sqlquery);
+            prepStatement.setInt(1,currentUserId);
+            resultSet = prepStatement.executeQuery();
+            String hashed ="";
+            if(resultSet.next()) hashed = resultSet.getString("password");
+            if(!BCrypt.checkpw(password, hashed)) return false;
+            prepStatement.close();
+            return true;
+        }catch (Exception ex){
+            System.out.println("Error in checkPassword"+ex);
+            return false;
+        }finally {
+            closeConnection(connection);
         }
     }
     private boolean isValidEmailAddress(String email) {
@@ -49,7 +69,15 @@ public class ProfileDB {
         java.util.regex.Matcher m = p.matcher(email);
         return m.matches();
     }
+    public static void closeConnection(Connection con) {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (Exception e) {
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println(new ProfileDB().createUser("pekka","pekka","pekka@pekkas.fi"));
+        System.out.println(new ProfileDB().checkPassword("pka",1));
     }
 }
